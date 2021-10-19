180f9e9b6569347e4875e5f2721f60ed2d3e938c
= <=>
2021-10-19 15:37:41 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,35 @@
+console.log("Merhaba")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+
+
+
+let konutKredileri = ["Konut kredisi", "Emlak kredisi", "Kamu kredisi"]
+
+
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+    
+}
+console.log("</ul")
+
+
+
+
+
